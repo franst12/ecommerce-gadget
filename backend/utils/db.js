bb22329f2c99af231d238f4a5e266fb3afea9af0
Drajet Franst <fransye12345@gmail.com>
2025-10-10 18:52:21 +0700

    mengubah koneksi database  mongoose

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -3,9 +3,12 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const db = mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const db = mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Database connected'))
+  .catch((err) => console.log(err));
 
 module.exports = db;
