ee3d5f169a0c7b8190855e97a70212d8e318be24
Drajet Franst <fransye12345@gmail.com>
2025-10-10 15:32:10 +0700

    update lahhhhh

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,11 @@ db.then(() => {
 // Rute API
 app.use('/api/products', productRoutes);
 app.use('/api/auth', authRoutes);
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    message: 'Selamat datang di server e-commerce',
+  });
+});
 
 app.use((req, res) => {
   return res.status(500).json({
